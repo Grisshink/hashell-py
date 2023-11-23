c44ebca0b6dcf732ad0683a21d804e26678edb78
Griss <grisshink@mail.ru>
2023-11-23 17:24:52 +0300

    Update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use pyo3::prelude::*;
 use ::hashell::hash_string as hashell_hash;
 
-/// Formats the sum of two numbers as string.
+/// Compute hash from provided string
 #[pyfunction]
 fn hash_string(inp: &str, hash_length: u32) -> PyResult<String> {
     Ok(hashell_hash(inp, hash_length))
 }
 
-/// A Python module implemented in Rust.
+/// Python bindings for hashell (see https://github.com/Grisshink/hashell)
 #[pymodule]
 fn hashell(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hash_string, m)?)?;
